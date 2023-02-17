cb9f4cc0baa0f7d18d8a33ef5bd4110fc5ac35d5
malpoly <marry2691@yandex.ru>
2023-02-17 12:35:25 +0300

    Write def title_letter.

--- str_func.py
+++ str_func.py
@@ -2,3 +2,8 @@ def upper_letter(offer=input()):
     '''Принимает на вход строку и возвращает ее со всеми заглавными буквами.'''
     offer_upper = offer.upper()
     print(offer_upper)
+
+def title_letter(offer=input()):
+    '''Делает заглавными первые буквы каждого слова в строке, поступившей на вход функции'''
+    offer_title = offer.title()
+    print(offer_title)
