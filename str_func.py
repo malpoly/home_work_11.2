0a0c30f0afbf53a57c8bbd1cbcb3937c78c2d353
malpoly <125284376+malpoly@users.noreply.github.com>
2023-02-17 12:29:14 +0300

    Update str_func.py
    Change docstring

--- str_func.py
+++ str_func.py
@@ -1,4 +1,4 @@
 def upper_letter(offer=input()):
-    '''Принимает на вход строку и возвращает ее со всеми заглавными буквами.'''
+    '''Увеличивает все буквы в строке'''
     offer_upper = offer.upper()
     print(offer_upper)
